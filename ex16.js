3163d73da809d7803cbe887838b5978b860a1ad6
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-09-29 16:17:21 -0700

    ex16 completed

--- ex16.js
+++ ex16.js
@@ -17,7 +17,111 @@ For more information on casing styles, read Wikipedia's Special Case Styles for 
 
 const makeCaze = function (input, caze) {
   // Put your solution here
+  let cazeArray = [];
+  let resultCaze = "";
+
+  if((typeof caze) !== "object") {
+    cazeArray.push(caze);
+  } else {
+    cazeArray = caze;
+  }
+
+  let newCaze = cazeArray.map((cazeItem, index) => {
+    if (index > 0) {
+      result = checkType(cazeItem, result);
+    } else {
+      result = checkType(cazeItem, input);
+    }
+    return result;
+  })
+
+  if (cazeArray.length > 1) {
+    newCaze = newCaze[cazeArray.length -1];
+  }
+
+  return newCaze.toString();
 };
+
+const checkType = function (caze, input) {
+  let text = "";
+  let splitText = [];
+  if (caze !== "upper" || caze !== "lover") {
+    splitText = input.split(" ");
+  }
+  
+  if (caze === "upper") {
+    text = input.toUpperCase(input);
+  } else if (caze === "lower") {
+    text = input.toLowerCase(input);
+  } else if (caze === "snake") {
+    text = splitText.join("_");
+  } else if (caze === "kebab") {
+    text = splitText.join("-");
+  } else {
+    text = makeUpperCase(splitText, caze);
+  }
+  // else if (caze === "pascal") {
+  //   text = makeUpperCase(splitText, caze);
+  // } else if (caze === "camel") {
+  //   text = makeUpperCase(splitText, caze); 
+  // } else if (caze === "title") {
+  //   text = makeUpperCase(splitText, caze);
+  // } else if (caze === "vowel") {
+  //   text = makeUpperCase(splitText, caze); 
+  // }
+
+  return text;
+}
+
+const makeUpperCase = function (input, type) {
+  let cazeWord = "";
+  const vowelCharacter = ["a", "e", "i", "o", "u"];
+
+  const resultArray = input.map((word, index) => {
+    if (type === "pascal" || type === "title") {
+      word = word.split("");
+      word[0] = word[0].toUpperCase();
+      word = word.join("");
+    } else if (type === "camel") {
+      if (index !== 0) {
+        word = word.split("");
+        word[0] = word[0].toUpperCase();
+        word = word.join("");
+      } else  {
+        word = word;
+      }
+    } else if (type === "vowel") {
+      word = word.split("");
+      word.forEach((element, index) => {
+        for(let i = 0; i < vowelCharacter.length; i++) {
+          if(element === vowelCharacter[i]) {
+            word[index] = element.toUpperCase();
+          }
+        }
+      });
+      word = word.join("");
+    } else if (type === "consonant") {
+      word = word.split("");
+      word.forEach((element, index) => {
+        for(let i = 0; i < vowelCharacter.length; i++) {
+          if(element.toLowerCase() === vowelCharacter[i]) {
+            word[index] = element.toLowerCase();
+          }
+        }
+      });
+      word = word.join("");
+    }
+    return word;
+  })
+
+  if (type === "title" || type === "vowel" || type === "consonant") {
+    cazeWord = resultArray.join(" ");
+  } else {
+    cazeWord = resultArray.join("");
+  }
+
+  return cazeWord;
+}
 
 console.log(makeCaze("this is a string", "camel")); // thisIsAString
 console.log(makeCaze("this is a string", "pascal")); // ThisIsAString
