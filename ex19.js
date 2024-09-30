3899315e96b83a1459da035742750ab0a7b1c92c
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-09-30 10:52:24 -0700

    ex19 completed

--- ex19.js
+++ ex19.js
@@ -66,3 +66,78 @@ Instruction
 Create a function generateBoard which will return a nested array representing the board, containing the location of two queens.
 Create a function called queenThreat that will indicate whether or not the two queens are positioned so that they attack each other.
 */
+
+let generateBoard = function (position1, position2) {
+  let board = [];
+  let row = [];
+
+  for(let i = 0; i < 8; i++) {
+    for(let j = 0; j < 8; j++) {
+      row[j] = 0;
+    }
+    board[i] = [...row];    
+  }
+
+  board = checkPosition(position1, board);
+  board = checkPosition(position2, board);
+
+  return board;
+}
+
+const checkPosition = function (position, board) {
+  const queen = position;
+
+  board[queen[0]][queen[1]] = 1;
+
+  return board;
+}
+
+const queenThreat = function (board) {
+
+  const queens = board.reduce((row, current, index) => {
+    current.forEach((element, elementIndex) => {
+      if(element === 1) {
+        if (row.length === 0) {
+          row = [[index, elementIndex]];
+        } else {
+          row.push([index, elementIndex]);
+        }
+      } else {
+        row = row;
+      }
+    });
+
+    return row = [...row];
+
+  },[])
+
+  const queen1y = queens[0][0];
+  const queen1x = queens[0][1];
+  const queen2y = queens[1][0];
+  const queen2x = queens[1][1];
+
+
+  if ((queen1x === queen2x) || (queen1y === queen2y)) {
+    return true;
+  } else if ((queen1x + queen1y) === (queen2x + queen2y)) {
+    console.log(queen1x + queen1y, queen2x + queen2y)
+    return true;
+  } else if ((queen1x - queen1y) === (queen2x - queen2y)) {
+    console.log(queen1x - queen1y, queen2x - queen2y)
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let whiteQueen = [0, 5];
+let blackQueen = [5, 0];
+let generatedBoard = generateBoard(whiteQueen, blackQueen);
+console.table(generatedBoard);
+console.log(queenThreat(generatedBoard));
+
+// let whiteQueen = [0, 0];
+// let blackQueen = [5, 7];
+// let generatedBoard = generateBoard(whiteQueen, blackQueen);
+// console.table(generatedBoard);
+// console.log(queenThreat(generatedBoard));
